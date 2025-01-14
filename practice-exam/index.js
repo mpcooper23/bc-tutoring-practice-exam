eb05df86d27bea959a0cf1bf52b64ce0a1aa5853
mpcooper23 <mpcooper23@gmail.com>
2025-01-14 13:15:29 -0600

    2

--- practice-exam/index.js
+++ practice-exam/index.js
@@ -1,19 +1,49 @@
 // Problem #1 // 
-function getYearlySubscribers(){
-   
+/**
+ * Create a function called `getYearlySubscribers` that takes in one parameter - `array` - 
+ * which represents an array of user objects like the one shown in data.js. This function 
+ * should use the native filter method to return a new array of only the user objects that 
+ * have a yearly subscription.
+ */
+
+function getYearlySubscribers(array){
+    return array.filter(user => {
+        if (user.subscription.type === 'Yearly')
+        return true
+    }
+    )
 }
 
 // Problem #2 //
+/**
+ * Create a function called getTheaterGoers that takes in one parameter - array - 
+ * which represents an array of user objects like the one shown in data.js. This 
+ * function should use the native filter method to return a new array of only the 
+ * user objects whose watched array includes a movie they have seen in theaters.
+
 function getTheaterGoers(){
     
 }
 
 // Problem #3 // 
+/**
+ *CCreate a function called mapUsers that takes in one parameter - array - 
+ which represents an array of user objects like the one shown in data.js. 
+ This function should use the native map method to return a new array of 
+ strings that includes the user's name followed by their subscription type 
+ and cost. Use the example below to format your strings. Note how 'yearly' 
+ and 'monthly' in the strings below are lowercase.
+ */
+
 function mapUsers(){
     
 }
 
 // Problem #4 //
+/**
+ * Create a function called `getYearlySubscribers` that takes in one parameter - `array` - which represents an array of user objects like the one shown in data.js. This function should use the native filter method to return a new array of only the user objects that have a yearly subscription.
+ */
+
 function getObjects(){
     
 }
