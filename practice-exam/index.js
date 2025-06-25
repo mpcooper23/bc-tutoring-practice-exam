ae770a6cb2c36fc59731636c419432435735a041
Michael Cooper <mpcooper23@gmail.com>
2025-06-25 11:59:30 -0500

    7-25

--- practice-exam/index.js
+++ practice-exam/index.js
@@ -5,16 +5,12 @@
  * should use the native filter method to return a new array of only the user objects that 
  * have a yearly subscription.
  * 
- * I:
+ * I: users array
  * O: array of users with yearly subs
  */
 
-function getYearlySubscribers(array){
-    return array.filter(user => {
-        if (user.subscription.type === 'Yearly')
-        return true
-    }
-    )
+function getYearlySubscribers (array) {
+ return array.filter(user => user.subscription.type === 'Yearly')
 }
 
 // Problem #2 //
@@ -28,14 +24,6 @@ function getYearlySubscribers(array){
  * O: array of user objects if watched array includes a theater film
 */
 
-function getTheaterGoers(array){
-    return array.filter(user => {
-        for(let i = 0; i < user.watched.length; i++){
-        if(user.watched[i].platform === 'Theater'){
-            return true;
-        }
-    }})
-}
 
 // Problem #3 // 
 /**
@@ -51,11 +39,6 @@ O: arr of template literals
 
  */
 
-function mapUsers(array){
-    return array.map(user => {
-        return `${user.name} has a ${user.subscription.type.toLowerCase()} subscription that costs ${user.subscription.cost}.`})
-
-}
 
 // Problem #4 //
 /**
@@ -65,16 +48,6 @@ function mapUsers(array){
  * looks like the example below:
  */
 
-function getObjects(array){
-    return array.map(user => {
-        return {
-            user: user.name,
-            location: `${user.location.city}, ${user.location.state}`,
-            watchedFilms: user.watched.length,
-            filmsInQueue: user.queue.length
-        }
-    })
-}
 
 // Problem #5 //
 /**
@@ -85,18 +58,7 @@ function getObjects(array){
  * the the name of a user followed by their email.
  */
 
-function getInfoByCity(array, city, output = []){
-    //base case
-    if(array.length === 0){
-        return output;
-    }
-    //check if user's city matches the input city
-    if(array[0].location.city === city && array[0].email){
-        output.push(`${array[0].name} - ${array[0].email}`);
-    }
-    //recursive call with the remaining array using slice
-    return getInfoByCity(array.slice(1), city, output)
-}
+
 
 // Problem #6 //
 
@@ -111,15 +73,5 @@ function getInfoByCity(array, city, output = []){
  * type marked "Montly", you will need to calculate what their monthly cost 
  * will add up over 12 months.
  */
-function getYearlyCost(array){
-    return array.reduce((acc, user) => {
-        if(user.subscription.type === 'Yearly'){
-            acc[user.name] = user.subscription.cost;
-        }else {
-            acc[user.name] = user.subscription.cost * 12
-        }
-return acc
-    }, {})
-   
-}
+
 
